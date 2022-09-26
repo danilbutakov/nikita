45c356695a66757dbf1c053e26be4f9d628df969
danilbutakov <12021109359@irgups.ru>
2022-09-26 18:20:56 +0800

    Небольшие изменения внешнего вида2

--- php/indexForm.php
+++ php/indexForm.php
@@ -18,17 +18,17 @@ require_once 'form.php';
     $form = mysqli_query($connect, "SELECT * FROM `form`");
     while ($resForm = mysqli_fetch_assoc($form)) {
     ?>
+        <span class="adminFormSpan">Все формы</span>
         <div class="adminForm__container">
-            <span class="adminFormSpan">Все формы</span>
             <form class="adminForm" action="" method="post">
                 <label for="name">Имя</label>
-                <input type="text" name="name" value="<?= $resForm['name']; ?>">
+                <input type="text" name="name" placeholder="<?= $resForm['name']; ?>">
                 <label for="number">Телефон</label>
-                <input type="text" name="phone" value="<?= $resForm['phone']; ?>">
+                <input type="text" name="phone" placeholder="<?= $resForm['phone']; ?>">
                 <label for="email">Email</label>
-                <input type="email" name="email" value="<?= $resForm['email']; ?>">
+                <input type="email" name="email" placeholder="<?= $resForm['email']; ?>">
                 <label for="message">Сообщение</label>
-                <textarea name="text" value="<?= $resForm['text']; ?>" cols="30" rows="10"></textarea>
+                <textarea name="text" placeholder="<?= $resForm['text']; ?>" cols="30" rows="10"></textarea>
             </form>
         </div>
     <?php
