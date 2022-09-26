7cd8abfed240266dd98ba380ce29ce22f52c005b
danilbutakov <12021109359@irgups.ru>
2022-09-26 18:10:27 +0800

    Изменено подключение к бд

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$connect = mysqli_connect("localhost", "root", "root", "site");
+$connect = mysqli_connect("localhost", "phpadmin", "123", "site");
 
 if (!$connect) {
     die('Error connect to DataBase');
